3437ff11689726a4d1de268f358b59211cf5d43f
jimmy de los angeles <jimmydelosangeles@me.com>
2016-12-13 23:16:18 -0500

    make branch to test state change

--- beerafide2.0/src/components/ListItem/ListItem.jsx
+++ beerafide2.0/src/components/ListItem/ListItem.jsx
@@ -7,9 +7,10 @@ class ListItem extends Component {
 
 
 
-<div className="list-item"  onClick={this.props.handleClickBeer(this.props.webID)}>
+<div className="list-item" id={this.props.webID} onClick={() => this.props.handleClickBeer(this.props.webID)} }>
+    <p>{this.props.webID}</p>
     <h2 className="name">{this.props.name}</h2>
-    <img src={this.props.pic} alt={this.props.name}/>
+    <img src={this.props.pic} alt={this.props.name} />
 
 </div>
     );
